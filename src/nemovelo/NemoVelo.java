a8b2b075864f57ce1c7abb62a66a42ba9cdfa16c
Yoldark34 <yoldark@gmail.com>
2013-04-08 22:27:11 +0200

    add view

--- src/nemovelo/NemoVelo.java
+++ src/nemovelo/NemoVelo.java
@@ -4,8 +4,11 @@
 */
 package nemovelo;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
-*
+ *
 * @author Admin
 */
 public class NemoVelo {
@@ -14,6 +17,14 @@ public class NemoVelo {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
+		view_user view = new view_user();
+		JFrame jf = new JFrame();
+		jf.setContentPane(view);
+		jf.setTitle("BorderTest");
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.pack();
+		jf.setResizable(false);
+		jf.setVisible(true);
 		// TODO code application logic here
 	}
 }
