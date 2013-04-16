76aa3d4a25acdcc96fb1ed042fe36e64c7298829
valentinseitz <valentinseitz@hotmail.fr>
2013-04-16 22:18:00 +0200

    Class to manage database element names

---
+++ src/model/database/DataBaseElementNames.java
@@ -0,0 +1,11 @@
+package model.database;
+
+/**
+ *
+ * @author Valentin SEITZ
+ */
+public class DataBaseElementNames {
+
+	public static final String BIKE = "bike";
+	public static final String BIKE_ID = "id";
+}
