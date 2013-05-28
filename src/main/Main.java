666a239d44d690b2a524906957fd99b49958bfa1
valentinseitz <valentinseitz@hotmail.fr>
2013-05-29 01:34:37 +0200

    Set look & feel, center frame on screen before display

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,8 @@
 package main;
 
 import controller.terminal.controller.TerminalController;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import resource.config.Configuration;
 import resource.log.ProjectLogger;
 import vue.terminal.TerminalMainFrame;
@@ -19,16 +21,35 @@ public class Main {
 		TerminalMainFrame mainFrame;
 		TerminalController controller;
 
+		//Configure Logger following tha configuration
 		ProjectLogger.setUniqueLog(
 				Configuration.getParam(
 				Configuration.CONFIGSECTION_LOG,
 				Configuration.CONFIGPARAM_LOG_UNIQUE)
 				.equals(Configuration.BooleanTrue));
 
+		//Set system look and feel
+		try {
+			UIManager.setLookAndFeel(
+					UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+
+		//Realy instanciate program
 		mainFrame = new TerminalMainFrame();
 		controller = new TerminalController(mainFrame);
 
-		mainFrame.setVisible(true);
+		//Centering the frame
+		mainFrame.setLocationRelativeTo(null);
 
+		//Display Vue
+		mainFrame.setVisible(true);
 	}
 }
