3125d80b725cebabfb33058fcfc7206b2bb36e6f
valentinseitz <valentinseitz@hotmail.fr>
2013-05-28 22:02:32 +0200

    Controller of TerminalWelcome

---
+++ src/controller/terminal/controller/TerminalWelcomeController.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.terminal.controller;
+
+/**
+ *
+ * @author Valentin SEITZ
+ */
+public class TerminalWelcomeController {
+}
