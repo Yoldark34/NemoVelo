0bd3843c83dd24671883201f6244525bc00ed67c
valentin.seitz <valentin.seitz@S2023PC16.ens-nime.ema.fr>
2013-05-28 17:14:39 +0200

    Add UsingController

---
+++ src/controller/using/interfacesGUI/UsingMainVue.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.using.interfacesGUI;
+
+/**
+ *
+ * @author valentin.seitz
+ */
+public class UsingMainVue {
+    
+}
