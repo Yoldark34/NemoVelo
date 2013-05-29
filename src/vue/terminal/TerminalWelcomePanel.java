2afd440ac6867cfe5d25c4a956ba9baadc561c46
valentin.seitz <valentin.seitz@S2023PC11.ens-nime.ema.fr>
2013-05-29 15:43:49 +0200

    WelcomeVue alows to reach Rent and Return vues

--- src/vue/terminal/TerminalWelcomePanel.java
+++ src/vue/terminal/TerminalWelcomePanel.java
@@ -4,8 +4,15 @@
  */
 package vue.terminal;
 
+import controller.terminal.controller.TerminalController;
+import controller.terminal.controller.TerminalWelcomeController;
 import controller.terminal.interfacesGUI.TerminalWelcome;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
@@ -13,6 +20,10 @@ import javax.swing.JPanel;
  * @author Valentin SEITZ
  */
 public class TerminalWelcomePanel extends JPanel implements TerminalWelcome {
+
+	private JPanel panelActions;
+	private JButton btnRent;
+	private JButton btnReturn;
 
 	public TerminalWelcomePanel(LayoutManager lm, boolean bln) {
 		super(lm, bln);
@@ -34,5 +45,34 @@ public class TerminalWelcomePanel extends JPanel implements TerminalWelcome {
 	}
 
 	private void initialize() {
+		this.setLayout(new BorderLayout());
+		this.panelActions = new JPanel();
+		{
+			this.panelActions.setLayout(new GridLayout(1, 2));
+			//Rent button
+			btnRent = new JButton("Louer");
+			{
+				btnRent.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent ae) {
+						TerminalWelcomeController.getTerminalWelcomeController().doRent();
+					}
+				});
+			}
+			this.panelActions.add(btnRent);
+
+			//Return button
+			btnReturn = new JButton("Rendre");
+			{
+				btnReturn.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent ae) {
+						TerminalWelcomeController.getTerminalWelcomeController().doReturn();
+					}
+				});
+			}
+			this.panelActions.add(btnReturn);
+		}
+		this.add(panelActions, BorderLayout.CENTER);
 	}
 }
