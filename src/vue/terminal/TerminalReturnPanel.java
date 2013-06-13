f3ab9395e62fba70946b7c0d0537e26100e3fdc7
valentinseitz <valentinseitz@hotmail.fr>
2013-06-13 23:31:48 +0200

    The return panel does use the panel which allows to manage several bike serial numbers

--- src/vue/terminal/TerminalReturnPanel.java
+++ src/vue/terminal/TerminalReturnPanel.java
@@ -6,12 +6,21 @@ package vue.terminal;
 
 import controller.terminal.controller.TerminalReturnController;
 import controller.terminal.interfacesGUI.TerminalReturn;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.HashSet;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 /**
  *
@@ -19,6 +28,10 @@ import javax.swing.JButton;
  */
 public class TerminalReturnPanel extends AbstractTerminalPanel implements TerminalReturn {
 
+	//Content
+	JComboBox cboNbBikes;
+	TerminalReturnBikesPanel panelBikes;
+	//Actions
 	private JButton btnReturn;
 
 
@@ -42,6 +55,63 @@ public class TerminalReturnPanel extends AbstractTerminalPanel implements Termin
 	}
 
 	private void initialize() {
+		GridBagConstraints gbc;
+
+		{//content
+			GridBagLayout gbl = new GridBagLayout();
+			{
+				gbl.columnWidths = new int[]{0, 0, 0};
+				gbl.rowHeights = new int[]{32, 0, 0};
+				gbl.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+				gbl.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+			}
+			this.getPanelContent().setLayout(gbl);
+
+			JLabel lblNombreDeVelos = new JLabel("Nombre de velos a rendre");
+			{//Position
+				gbc = new GridBagConstraints();
+				gbc.insets = new Insets(0, 0, 5, 5);
+				gbc.anchor = GridBagConstraints.WEST;
+				gbc.gridx = 0;
+				gbc.gridy = 0;
+			}
+			this.getPanelContent().add(lblNombreDeVelos, gbc);
+
+			this.cboNbBikes = new JComboBox();
+			{
+				this.cboNbBikes.addItemListener(new ItemListener() {
+					@Override
+					public void itemStateChanged(ItemEvent ie) {
+						panelBikes.setBikeQuantity(((Integer) cboNbBikes.getSelectedItem()).intValue());
+					}
+				});
+				{//Position
+					gbc = new GridBagConstraints();
+					gbc.insets = new Insets(0, 0, 5, 0);
+					gbc.fill = GridBagConstraints.BOTH;
+					gbc.gridx = 1;
+					gbc.gridy = 0;
+				}
+			}
+			add(this.cboNbBikes, gbc);
+
+			JScrollPane scrollPane = new JScrollPane();
+			{
+				this.panelBikes = new TerminalReturnBikesPanel();
+				//The view port contains the panel which allows to handle bikes
+				scrollPane.setViewportView(this.panelBikes);
+
+				{//Position
+					gbc = new GridBagConstraints();
+					gbc.gridwidth = 2;
+					gbc.insets = new Insets(0, 0, 0, 5);
+					gbc.fill = GridBagConstraints.BOTH;
+					gbc.gridx = 0;
+					gbc.gridy = 1;
+				}
+			}
+			this.getPanelContent().add(scrollPane, gbc);
+		}
 
 		{//Actions
 			this.getPanelActions().setLayout(new GridLayout(1, 1));
@@ -57,6 +127,14 @@ public class TerminalReturnPanel extends AbstractTerminalPanel implements Termin
 				});
 			}
 			this.getPanelActions().add(this.btnReturn);
+		}
+	}
+
+	@Override
+	public void init() {
+		this.cboNbBikes.removeAllItems();
+		for (int i = 1; i <= TerminalReturnController.getTerminalReturnController().getMaxAvailableStorages(); i++) {
+			this.cboNbBikes.addItem(new Integer(i));
 		}
 	}
 }
